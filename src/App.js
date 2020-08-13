20d9dae3018bfeae01c53fb19c478e34202dc844
Hicham <hichoh00@gmail.com>
2020-08-13 16:09:50 -0400

    add code

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Game from '.Game/components/game/Game';
+import './App.css';
+import raccoon from './assets/images/raccoon.png'
+import squirrel from './assets/images/squirrel.png'
+import bunny from './assets/images/bunny.png'
+import hound from './assets/images/hound.png'
+
+
+
+
+function App(props) {
+  
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: raccoon
+  }
+
+  const squirrels = {
+    name: 'Sheridan Squirrels',
+    logoSrc: squirrel
+  }
+
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: bunny
+  }
+
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: hound
+  }
+
+  return (
+    <div className="App">
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
+    </div>
+  )
+}
+  
+export default App;
